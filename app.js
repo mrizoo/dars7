5e0c26870dc5e42faa9ecd684c8147f300b62ec0
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-02-27 15:10:07 +0500

    add big js issues

--- app.js
+++ app.js
@@ -132,3 +132,214 @@
 // }else{
 //     alert(`Siz ${meyor} ta belgi kiritishingiz mumkin, va hozirda siz ${belgilar.length} ta belgi kiritdingiz.Ya'na ${belgilar.length - meyor} ta belgi kiritishingiz mumkin `)
 // }
+
+// array mashqlari 2
+// 1 mashq
+// arraydagi stringni indexni topish
+
+// const myArray =[[false,true], [1,2], 99 ,`yaxshi`]
+
+//for
+// for( let i = 0; i < myArray.length; i++) {
+//   if (typeof  myArray[i] == `string`){
+//     console.log(`string index:`, i);
+//   }
+// }
+
+// myArray.forEach((item, i) =>{
+//   if (typeof item == `string`){
+//     console.log(`string index:`,i );
+//   }
+// })
+
+// 2 mashq
+// Array ichidagi fqat arraylarni Elementlarini ajratib beradi
+
+// const data = [[1,2,3,], `qizil`, [4,5,6], true, false,]
+// const newData = []
+// data.forEach((item)=>{
+//   const res = Array.isArray(item)
+//   if(res){
+//     item.forEach((num)=>{
+//       newData.push()
+//     })
+//   }
+// })
+// console.log(newData);
+
+// 3 mashq
+// array ichidagi [null, 0, false, undefined ] qiymatlarini ajratib oling
+
+// const checkData = [null,`false`,0,99, false,undefined,'',NaN, ]
+
+// const falsey = []
+
+// checkData.forEach((item) =>{
+//   if(!item){
+//     falsey.push(item)
+//   }
+// })
+// console.log(falsey);
+
+// 4 mashq
+// berilgan qiymar samarqand :
+// kutilayotgan qiymat dnaqramas
+
+//  1 - usul
+//  const word = `Samarqand`
+//  const splitWord = word.split('').reverse()
+// let newWord = ''
+//  for (let i = 0; i<splitWord.length; i++) {
+//   newWord += splitWord[i]
+//  }
+
+//  console.log(newWord);
+
+// 2 usul
+// const word = `Samarqand`
+// const splitWord = word.split('')
+// let newWord = ''
+
+// for(let i = word.length-1;i >=0; i--){
+//   newWord += splitWord[i]
+// }
+// console.log(newWord);
+
+// 5 mashq
+// promptadn kiritilgan istalgan suzlarni suzlarni birinchi harfi kichik qoganlari katta chiqarish kerak
+
+// const word = propmt`Biror gap kiriting`
+// const splitWord = word.split(' ')
+// let newWord = ''
+
+
+// splitWord.forEach((item)=>{
+//   const boshi = item.charAt().toLowerCase()
+//   const davomi = item.slice(1).toUpperCase()
+
+//   newWord += ` ` + (boshi + davomi)
+// })
+
+// console.log(newWord.trimStart());
+
+// 6 mashq
+// array ichidagi natural raqamlar yigindisini toping
+// const numbers = [1,2.5,-9,89,-9,45,Infinity,2,0]
+// naturalNumber = 0
+// numbers.forEach((num)=>{
+//   if(num>=0.0 && Math.floor(num) === num && num!== Infinity && num !== 0){
+//     naturalNumber +=num
+//   }
+// })
+// console.log(naturalNumber);
+
+// 7 mashq
+// promptdan bitta suz kiritilgan ichidagi 'a' harfi necha marta ishlatilanini aniqlang
+
+// const word = `salom alekum bratim `
+// let splitWord = word.split ('')
+// let counter = 0 
+
+
+// splitWord.forEach((words) => {
+//   if(words == `a` || words == `A`){
+//     counter++
+//   }
+// })
+// console.log(counter);
+
+
+// 8 mashq
+
+// const types = [21.1,`Roziya`,`arrow`,[`I am  array`], null, true,245]
+// const newTypes = []
+
+
+// types.forEach((types)=>{
+//   newTypes.push(typeof types)
+// })
+// console.log(newTypes);
+
+// 9 mashq
+
+// kiritilgan textni uzunligini aniqlang length dan foydalanmang
+
+// const word = `salom alekum bratim`
+// console.log(word.lastIndexOf(word.slice(-1))+1);
+
+// 10 mashq
+
+// const number = [1,2,3,4,5,7]
+// let result = []
+
+// number.forEach((num,i)=>{
+//   result.push(num + 1)
+// })
+
+// console.log(result);
+
+// 11 mashq tarkinidagi yigindsi
+
+//  let counter = 0
+//  for ( let  i = 0; i <= 10; i++){
+//   counter +=i
+ 
+//  }
+//  console.log(counter);
+
+// 12 mashq 
+
+// let num = 9
+
+// for(i = 1; i<=10;i++){
+//  result =  num * i +1
+// }
+// console.log(result);
+
+// 13 mashq
+// let numbers = [1,2,3,4,5]
+// let result = []
+
+// const reverseFunc = (arr) => {
+//   arr.forEach(num => {
+//    result.push(num * -1)
+//   })
+// }
+// reverseFunc(numbers)
+// console.log(result);
+
+// 14 mashq
+//  const result = []
+// const fizzBuzzFunc = function (number) {
+//    for ( let i =1; i<=number; i++){
+//     if(i % 3 == 0 && i % 5==0){
+//       result.push(`FizzBuzz`)
+//     }else if (i % 3 == 0){
+//       result.push(`Fizz`)
+//     }else if( i % 5 == 0 ){
+//       result.push(`Buzz`)
+//     }else {
+//       result.push(i)
+//     }
+//    }
+// }
+
+// fizzBuzzFunc(30)
+// console.log(result);
+
+// 15 mashq
+
+// const MuhammadrizaObj = {
+//   name: `Muhammadrizo`,
+//   lastName: `Pulatov`,
+//   age: 19,
+//   region: `Fergana`,
+//   isMerried: false , 
+//   job: `Programmer`,
+
+//   haqida: function( ){
+//     return `
+//     Assalomu alekum, Mening ismim ${this.name}, Familyam ${this.lastName}. Yoshim ${this.age}da. ${this.region} fa yashayman. ${this.job} bo'lib faoliyat olib boraman. ${this.isMerried ? "Uylanganman" : "Hali uylanmaganman"}
+//     `
+//   }
+// }
